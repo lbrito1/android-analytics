c812c2e66baa4a3381a515de85a8c4205e481951
Leonardo Brito <lbrito@gmail.com>
2020-06-17 19:40:58 -0300

    Fixes constraint.

--- db/migrations/2_add_constraints.rb
+++ db/migrations/2_add_constraints.rb
@@ -1,9 +1,9 @@
 class AddConstraints < Sequel::Migration
   def up
     alter_table :hits do
-      add_constraint(:url_length) { char_length(:url) < 256 }
+      add_constraint(:url_length) { char_length(:url) < 128 }
       add_constraint(:ip_length) { char_length(:ip) < 16 }
-      add_constraint(:user_agent_length) { char_length(:user_agent) < 128 }
+      add_constraint(:user_agent_length) { char_length(:user_agent) < 256 }
     end
   end
 
