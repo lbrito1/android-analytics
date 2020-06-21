d4a5a8598e5975f841b4be9103f7442709fd6331
Leonardo Brito <lbrito@gmail.com>
2020-06-21 16:00:58 -0300

    Update script

---
+++ bin/update_countries.sh
@@ -0,0 +1,8 @@
+#!/data/data/com.termux/files/usr/bin/sh
+cd /data/data/com.termux/files/home/android-sinatra
+ruby -e "File.write('bbb', 'bbb')"
+# ruby -e "require_relative ''"
+
+
+# class Compiler
+#   def self.update_geo_info
