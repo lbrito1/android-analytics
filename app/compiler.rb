abfa7d5a82efbdad6ab6a71c0ea3c5ca276af379
Leonardo Brito <lbrito@gmail.com>
2020-06-21 14:26:38 -0300

    Rewrites compiler.

--- app/compiler.rb
+++ app/compiler.rb
@@ -3,40 +3,13 @@ require 'csv'
 require 'digest'
 require 'geocoder'
 require_relative '../config/db'
+require_relative './models/hits'
 
-OUTPUT_FILE = './log/output'
+Hits.where(country: nil).each do |hit|
+  next unless geo_info = Geocoder.search(hit.ip).first
 
-MAIN_LOG_FILE_PATH = './log/nginx.access.log'
-BACKUP_LOG_FILE_PATH = './log/nginx.access.log.bak'
-HEADERS = %i(ip remote_user created_at request status body_bytes_sent url
-  user_agent http_x_forwarded_for request_time upstream_response_time)
+  geo_info = geo_info.data.slice(%w(country region city)
+  anonnymized_ip = Digest::MD5.hexdigest(hit.ip)
 
-def write_to_output(string)
-  File.open(OUTPUT_FILE, 'a') { |f| f.write("\n#{Time.now} - #{string}") }
+  hit.update(geo_info.merge(ip: anonnymized_ip))
 end
-
-logs = File.read(MAIN_LOG_FILE_PATH)
-csv = CSV.new(logs, headers: HEADERS)
-
-return write_to_output("No logs found.") if logs.size.zero?
-csv_count = 0
-csv.each do |row|
-  db_entry = row.to_h.slice(:ip, :created_at, :url, :user_agent)
-  ip = db_entry[:ip]
-
-  if (geo_info = Geocoder.search(ip).first)
-    db_entry[:country] = geo_info.country
-    db_entry[:region] = geo_info.region
-    db_entry[:city] = geo_info.city
-  end
-  # TODO: OS, device
-  db_entry[:ip] = Digest::MD5.hexdigest(db_entry[:ip]) # anonnymize IP
-
-  DB[:hits].insert(db_entry)
-  csv_count += 1
-end
-
-File.open(BACKUP_LOG_FILE_PATH, 'a') { |f| f.write(logs) }
-File.write(MAIN_LOG_FILE_PATH, '')
-
-write_to_output("Wrote #{csv_count} rows to DB.")
