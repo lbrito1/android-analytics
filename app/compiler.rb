958d286b301d9b7a5b27940bce19b72b59e3d92e
Leonardo Brito <lbrito@gmail.com>
2020-06-21 13:42:23 -0300

    Fixes compiler.

--- app/compiler.rb
+++ app/compiler.rb
@@ -12,14 +12,14 @@ HEADERS = %i(ip remote_user created_at request status body_bytes_sent url
   user_agent http_x_forwarded_for request_time upstream_response_time)
 
 def write_to_output(string)
-  File.open(OUTPUT_FILE, 'a') { |f| f.write(string) }
+  File.open(OUTPUT_FILE, 'a') { |f| f.write("\n#{Time.now} - #{string}") }
 end
 
 logs = File.read(MAIN_LOG_FILE_PATH)
 csv = CSV.new(logs, headers: HEADERS)
 
-return write_to_output("#{Time.now} No logs found.") if logs.size.zero?
-
+return write_to_output("No logs found.") if logs.size.zero?
+csv_count = 0
 csv.each do |row|
   db_entry = row.to_h.slice(:ip, :created_at, :url, :user_agent)
   ip = db_entry[:ip]
@@ -33,9 +33,10 @@ csv.each do |row|
   db_entry[:ip] = Digest::MD5.hexdigest(db_entry[:ip]) # anonnymize IP
 
   DB[:hits].insert(db_entry)
+  csv_count += 1
 end
 
 File.open(BACKUP_LOG_FILE_PATH, 'a') { |f| f.write(logs) }
 File.write(MAIN_LOG_FILE_PATH, '')
 
-write_to_output("#{Time.now} Wrote #{csv.count} rows to DB.") if logs.size.zero?
+write_to_output("Wrote #{csv_count} rows to DB.")
