9c72a49714c12f5302d5a639f7436a1efeaddcb4
Leonardo Brito <lbrito@gmail.com>
2020-06-22 21:13:57 -0300

    Fixes require.

--- config/db.rb
+++ config/db.rb
@@ -1,6 +1,5 @@
 require 'sequel'
 require_relative '../.test_credentials_secret' # Test environment only
-require_relative './models/hits'
 
 DB = Sequel.connect(
   adapter: :postgres,
@@ -8,3 +7,5 @@ DB = Sequel.connect(
   host: ENV['ANDROID_DATABASE_HOST'],
   user: ENV['ANDROID_DATABASE_USER'],
   password: ENV['ANDROID_DATABASE_PASSWORD'])
+
+require_relative '../app/models/hits'
