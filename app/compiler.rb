514ef85be694dc8c622a120ce875338a504bc1f1
Leonardo Brito <lbrito@gmail.com>
2020-06-28 17:14:58 -0300

    Updates Compiler.

--- app/compiler.rb
+++ app/compiler.rb
@@ -1,22 +1,67 @@
 require 'digest'
 require 'geocoder'
-require_relative '../config/db'
+require 'csv'
+require 'byebug'
 
 class Compiler
-  def self.update_geo_info
-    query = Hits.where(country: nil)
-    count = query.count
-    i = 0
-    query.each do |hit|
-      puts "Updating hit from #{hit.ip}... (#{i += 1}/#{count})"
-      next unless geo_info = Geocoder.search(hit.ip).first
+  HEADERS = %i(ip created_at request status body_bytes_sent url user_agent
+    http_x_forwarded_for request_time)
+  OUTPUT_FILE = './log/compiler'
 
-      hit.update(
-        country: geo_info.country,
-        region: geo_info.region,
-        city: geo_info.city,
-        ip: Digest::MD5.hexdigest(hit.ip)
-      )
+  def initialize(path)
+    @path = path
+  end
+
+  def process
+    return write_to_output("No logs found.") if csv.none?
+
+    errors = []
+    csv.each do |row|
+      hash = row.to_h.slice(:ip, :created_at, :url, :user_agent)
+      hash[:created_at] = Time.strptime((hash[:created_at].to_f * 1000).to_s, '%Q')
+
+      geo = geo_info(hash[:ip])
+      hash.merge!(geo) if geo
+      # TODO: OS, device
+      hash[:ip] = Digest::MD5.hexdigest(hash[:ip]) # anonnymize IP
+
+      hit = Hits.create(hash)
+      errors << hit.errors.full_messages.to_sentence if hit.invalid?
+    end
+
+    if errors.any?
+      errors = errors.join("\n")
+      error_msg = "There were errors. \n#{errors}"
+      write_to_output(error_msg)
+    end
+
+    write_to_output("Wrote #{csv.count} rows to DB.")
+  end
+
+  private
+
+  def geo_info(ip)
+    return unless (geo = Geocoder.search(ip).first)
+
+    {
+      country:  geo.country,
+      region:  geo.region,
+      city:  geo.city,
+      lat:  geo.latitude,
+      long:  geo.longitude,
+    }
+  end
+
+  def csv
+    @csv ||= begin
+      logs = File.read(@path)
+      CSV.new(logs, headers: HEADERS).to_a
     end
   end
+
+  def write_to_output(string)
+    File.open(OUTPUT_FILE, 'a') { |f| f.write("\n#{Time.now} - #{string}") }
+  end
 end
+
+
--- bin/update_countries.sh
+++ bin/update_countries.sh
@@ -1,6 +1,15 @@
 #!/data/data/com.termux/files/usr/bin/sh
-PROJECT=/data/data/com.termux/files/home/android-sinatra
+PROJECT=/data/data/com.termux/files/home/android-analytics
 BUNDLE=/data/data/com.termux/files/usr/bin/bundle
+NGINX_PID=/data/data/com.termux/files/home/android-analytics/log/nginx.pid
 cd $PROJECT
 
-$BUNDLE exec ruby -e "File.write('log/compiler.log', ''); require '$PROJECT/app/compiler'; Compiler.update_geo_info"
+TIMESTAMP=`date '+%s'`
+LATEST_LOG_FILE=log/nginx.access.log.$TIMESTAMP
+
+# logrotate
+mv log/nginx.access.log $LATEST_LOG_FILE
+kill -USR1 `cat $NGINX_PID`
+sleep 3
+
+$BUNDLE exec ruby -e "File.write('log/compiler.log', ''); require '$PROJECT/app/compiler'; Compiler.new($LATEST_LOG_FILE).process"
