ac4487f6cedc2483553e3af60ab32b28aef68fec
Leonardo Brito <lbrito@gmail.com>
2020-06-28 15:49:21 -0300

    Fixes script.

--- bin/run.sh
+++ bin/run.sh
@@ -18,9 +18,9 @@ then
   echo "Starting postgres..."
   pg_ctl -D $PREFIX/var/lib/postgresql start
 fi
-BUNDLE install --path vendor/bundle
+$BUNDLE install --path vendor/bundle
 
 mkdir -p tmp/pids
 mkdir -p tmp/puma
 
-RAILS_ENV=production BUNDLE exec PUMA -C config/puma.rb
+RAILS_ENV=production $BUNDLE exec $PUMA -C config/puma.rb
