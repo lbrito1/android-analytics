f485d5f569f9b3c16964d887f5e174bf7308260d
Leonardo Brito <lbrito@gmail.com>
2020-06-28 16:01:53 -0300

    Updates config.

--- bin/run.sh
+++ bin/run.sh
@@ -2,15 +2,7 @@ BUNDLE=`which bundle`
 PUMA=`which puma`
 ln -sf `pwd`/config/nginx.conf $PREFIX/etc/nginx/nginx.conf
 
-if pgrep -x "nginx" > /dev/null
-then
-  echo "Reloading nginx config..."
-  nginx -s reload
-else
-  echo "Starting nginx..."
-  pg_ctl -D $PREFIX/var/lib/postgresql start
-fi
-
+killall nginx
 nginx
 
 if ! pgrep -x "postgres" > /dev/null
