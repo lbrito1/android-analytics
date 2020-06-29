7b727a6109d77f5d7fe96e4614cc728353d02a20
Leonardo Brito <lbrito@gmail.com>
2020-06-29 20:37:25 -0300

    Fixes creds.

--- config/db.rb
+++ config/db.rb
@@ -1,11 +1,11 @@
 require 'sequel'
-require_relative '../.test_credentials_secret' # Test environment only
+require 'byebug'
 
 DB = Sequel.connect(
   adapter: :postgres,
-  database: android_analytics_production,
-  host: localhost,
-  user: android_analytics
+  database: 'android_analytics_production',
+  host: 'localhost',
+  user: 'android_analytics',
   password: ENV['ANDROID_DATABASE_PASSWORD'])
 
 require_relative '../app/models/hits'
