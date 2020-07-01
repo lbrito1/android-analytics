3eaa64763502a77908a93f2d45f10a48e5c22ec2
Leonardo Brito <lbrito@gmail.com>
2020-07-01 19:20:49 -0300

    Moves around .env.

--- bin/run.sh
+++ bin/run.sh
@@ -1,13 +1,18 @@
 BUNDLE=`which bundle`
 
+# Copy config files
 ln -sf `pwd`/config/nginx.conf $PREFIX/etc/nginx/nginx.conf
-ln -sf `pwd`/viewer/.env `pwd`/.env
+ln -sf `pwd`/.env `pwd`/viewer/.env
+
+# Restart nginx
 killall nginx
 nginx
 
+# Restart postgresql
 if ! pgrep -x "postgres" > /dev/null
 then
   echo "Starting postgres..."
   pg_ctl -D $PREFIX/var/lib/postgresql start
 fi
+
 $BUNDLE install --path vendor/bundle
