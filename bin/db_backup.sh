7abddb12d9a155e8d57c18b23e3ee45c8e680583
Leonardo Brito <lbrito@gmail.com>
2020-07-04 11:05:01 -0300

    Update

--- bin/db_backup.sh
+++ bin/db_backup.sh
@@ -3,6 +3,4 @@ source .env
 [[ -z "$DB_USERNAME" ]] && { echo "Failed: DB_USERNAME was not found." ; exit 1; }
 [[ -z "$DB_PASSWORD" ]] && { echo "Failed: DB_PASSWORD was not found." ; exit 1; }
 
-pg_dump -U $DB_USERNAME --data-only --column-inserts -t blazer_queries > ../backup.sql
-
-
+pg_dump -U $DB_USERNAME -d $DB_NAME --data-only --column-inserts -t blazer_queries > backup.sql
