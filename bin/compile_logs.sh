98e823b72e9236156248bdea3aa7c8fdfb8a23b0
Leonardo Brito <lbrito@gmail.com>
2020-07-04 09:51:47 -0300

    Update

--- bin/compile_logs.sh
+++ bin/compile_logs.sh
@@ -2,8 +2,8 @@
 PROJECT=/data/data/com.termux/files/home/android-analytics
 BUNDLE=/data/data/com.termux/files/usr/bin/bundle
 NGINX_PID=/data/data/com.termux/files/home/android-analytics/log/nginx.pid
-cd $PROJECT
 
+cd $PROJECT
 
 mkdir -p log/old
 TIMESTAMP=$(date +%Y%m%d_%H%M%S%Z)
@@ -14,4 +14,7 @@ mv log/nginx.access.log $LATEST_LOG_FILE
 kill -USR1 `cat $NGINX_PID`
 sleep 3
 
+mkdir -p log/debug
+touch log/debug/"$TIMESTAMP"_start
 $BUNDLE exec ruby -e "require '$PROJECT/app/compiler'; Compiler.new('$LATEST_LOG_FILE').process"
+touch log/debug/"$TIMESTAMP"_finish
