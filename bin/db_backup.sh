f096c6607807de50b9e09acf334cc9b1275bfe2c
Leonardo Brito <lbrito@gmail.com>
2020-07-04 11:01:24 -0300

    Update

--- bin/db_backup.sh
+++ bin/db_backup.sh
@@ -1,4 +1,4 @@
-source ../.env
+source .env
 [[ -z "$DB_NAME" ]] && { echo "Failed: DB_NAME was not found. (source .env first)" ; exit 1; }
 [[ -z "$DB_USERNAME" ]] && { echo "Failed: DB_USERNAME was not found." ; exit 1; }
 [[ -z "$DB_PASSWORD" ]] && { echo "Failed: DB_PASSWORD was not found." ; exit 1; }
