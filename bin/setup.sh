f30bb5de83a5a623d60ccdb1e3a5689d2676e9f3
Leonardo Brito <lbrito@gmail.com>
2020-07-04 10:20:46 -0300

    Update

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,8 +1,12 @@
 WD=`pwd`
+
+# DB settings
 DB_NAME=`cat .env | sed -n -e 's/^DB_NAME=//p'`
 DB_USERNAME=`cat .env | sed -n -e 's/^DB_USERNAME=//p'`
 DB_PWD=`cat .env | sed -n -e 's/^DB_PASSWORD=//p'`
-[[ -z "$DB_PWD" ]] && { echo "Failed: DB_PASSWORD was not found." ; exit 1; }
+[[ -z "$DB_NAME" ]] && { echo "Failed: DB_NAME was not found." ; exit 1; }
+[[ -z "$DB_USERNAME" ]] && { echo "Failed: DB_USERNAME was not found." ; exit 1; }
+[[ -z "$DB_PWD" ]] && { echo "Failed: DB_PWD was not found." ; exit 1; }
 
 # Copy config files
 ln -sf $WD/.env $WD/viewer/.env
