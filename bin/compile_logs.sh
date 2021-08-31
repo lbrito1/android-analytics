f04131923e021630ed7c01ade1c36dc5d0dc928b
Leonardo Brito <lbrito@gmail.com>
2021-08-31 14:28:57 -0300

    Improves compile_log script.

--- bin/compile_logs.sh
+++ bin/compile_logs.sh
@@ -6,15 +6,19 @@ NGINX_PID=/data/data/com.termux/files/home/android-analytics/log/nginx.pid
 cd $PROJECT
 
 mkdir -p log/old
-TIMESTAMP=$(date +%Y%m%d_%H%M%S%Z)
-LATEST_LOG_FILE=log/old/nginx.access.log.$TIMESTAMP
+# TIMESTAMP=$(date +%Y%m%d_%H%M%S%Z)
+
+LOG_FILE=log/nginx.access.log
+BACKUP_FILE=log/old/nginx.access.log.bak
+TEMP_FILE=log/nginx.access.log.tmp
 
 # logrotate
-mv log/nginx.access.log $LATEST_LOG_FILE
+cat $LOG_FILE >> $BACKUP_FILE
+mv $LOG_FILE $TEMP_FILE
 kill -USR1 `cat $NGINX_PID`
 sleep 3
 
-mkdir -p log/debug
-touch log/debug/"$TIMESTAMP"_start
-$BUNDLE exec ruby -e "require '$PROJECT/app/compiler'; Compiler.new('$LATEST_LOG_FILE').process"
-touch log/debug/"$TIMESTAMP"_finish
+# mkdir -p log/debug
+# touch log/debug/"$TIMESTAMP"_start
+$BUNDLE exec ruby -e "require '$PROJECT/app/compiler'; Compiler.new('$TEMP_FILE').process"
+# touch log/debug/"$TIMESTAMP"_finish
